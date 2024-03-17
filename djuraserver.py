4363b6604b61549acc384709981625f3a7913380
Boris Milosavljevic <boris.milosavljevic31@gmail.com>
2024-03-17 21:53:54 +0100

    city as a param and cors added to backend code

--- djuraserver.py
+++ djuraserver.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -10,9 +12,10 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80),nullable=False)
     password = db.Column(db.String(120), nullable=False)
     number = db.Column(db.Integer,nullable=False)
+    city = db.Column(db.String(80))
 
     def __repr__(self):
         return f'<User {self.username} {self.number} >'
@@ -25,11 +28,11 @@ def login():
     data = request.get_json()
     user = User.query.filter_by(username=data['username']).first() 
     if not user:
-        return False 
+        return jsonify({'message': 'Password and Username combination incorrect'})
     if user.password == data['password'] :
-        return jsonify({'username': user.username,"number":user.number , "id":user.id})
+        return jsonify({'username': user.username,"number":user.number , "id":user.id,"city":user.city})
     else:
-         return jsonify({'message': 'Password incorrect'})
+         return jsonify({'message': 'Password and Username combination incorrect'})
     
 @app.route('/savedata',methods=["POST"])
 def saveData():
